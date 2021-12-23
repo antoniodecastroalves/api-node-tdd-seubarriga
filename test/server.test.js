a97f79680923bd5d100c4e7f3bef6927836a66a5
Antonio Alves <antoniodecastroalves@gmail.com>
2021-12-23 02:28:03 -0300

    should be able to respond at the root

--- test/server.test.js
+++
@@ -1,8 +0,0 @@
-const supertest = require('supertest');
-
-const request = supertest('http://localhost:3001');
-
-test('should be able to respond on port 3001 ', () => {
-  return request.get('/')
-    .then((res) => expect(res.status).toBe(200));
-});
